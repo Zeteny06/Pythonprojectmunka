7f12c61b2c6fe6d3f87821a0138e04ba3afcdd81
Zeteny06 <blummel.zeteny@students.jedlik.eu>
2022-11-10 08:34:37 +0100

    menu+adatok

---
+++ main.py
@@ -0,0 +1,6 @@
+from menu import menu
+
+
+choice = -1
+while choice != 0:
+    choice = menu()
---
+++ menu.py
@@ -0,0 +1,11 @@
+def menu():
+    option = -1
+    while option < 0 or option > 5:
+        print("0-kilépés")
+        print("1-Listázás")
+        print("2-Keresés")
+        print("3-új")
+        print("4-törlés")
+        print("5-Módosítás")
+        option = int(input("Válasszon a fentiek közül"))
+    return option
