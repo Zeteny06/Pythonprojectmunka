7fa908ca5b7e5961d0c863ea54a4256ebcb317a8
Zeteny06 <blummel.zeteny@students.jedlik.eu>
2022-11-17 18:48:29 +0100

    minor changes

--- randomgroups.py
+++ randomgroups.py
@@ -7,6 +7,13 @@ def groups(csapatok, Csoportszam):
     for index in range(Csoportszam):
         group = csapatok[index::Csoportszam]
         all_groups.append(group)
+    print(all_groups)
     for index, group in enumerate(all_groups):
         print(f"Csoport {index+1}: {' / '.join(group)}\n")
+    f = open("csoportok.csv","w",encoding="UTF8")
+    for row in all_groups:
+        y = ""
+        for x in row:
+            y += x + ";"
+        f.writelines(y.strip(";")+"\n")
     return all_groups
