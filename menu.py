e8c96af88bd135e2a46ff6f2e5b6d67428877e09
KaldiMartin1 <91196034+KaldiMartin1@users.noreply.github.com>
2022-11-23 12:14:03 +0100

    menu #6

--- menu.py
+++ menu.py
@@ -1,11 +1,9 @@
 def menu():
     option = -1
-    while option < 0 or option > 5:
+    while option < 0 or option > 3:
         print("0-kilépés")
         print("1-Random csoport készítés")
         print("2-Keresés")
-        print("3-új")
-        print("4-törlés")
-        print("5-Módosítás")
+        print("3-Szimulálás")
         option = int(input("Válasszon a fentiek közül:"))
     return option
